35c9c93a89cf395cb7f4aca0c9dfc622b3542f61
Evan Busse <evanbusse@gmail.com>
2017-05-05 15:55:27 -0600

    sectio header working correctly

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,24 @@
-$(function () {
+$(document).ready(function() {
+
+  $('#fullpage').fullpage({
+    anchors:['home', 'work', 'about'],
+    sectionsColor : ['#FF686B', '#ffe'],
+    loopBottom: true,
+    afterLoad: function(anchorLink, index){
+      var loadedSection = $(this);
+      console.log($(window).height());
+
+      //make sectio title follow page
+      if(index !== 1){
+        $('#sectionTitle').show();
+        $('#title').text(anchorLink);
+        $('#sectionTitle').animate({
+          top: $(window).height() * (index - 1) + 10
+        },300)
+      }
+
+    }
+  });
 
   $('#down_icon').delay(300).fadeIn(300);
-
 });
