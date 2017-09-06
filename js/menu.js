57869e2492a0ad3549fec11bd3630f8fffb1604d
Evan Busse <evanbusse@gmail.com>
2017-09-06 16:40:18 -0600

    menu expanding and retracting

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,15 @@
 (function(){
   console.log('Menu Initialized');
+  var $menu = $('#menu')
+
+  $('#menu-tab').on('click', function(e){
+    var value = 0
+    if($menu.css('left') === '0px'){
+      value = -250
+    }
+    $menu.animate({
+      left: `${value}px`
+    }, 500)
+  })
+
 }())
