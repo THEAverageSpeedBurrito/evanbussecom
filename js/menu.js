07a275fa6b29836b487d399d9acf79120990906f
Evan Busse <evanbusse@gmail.com>
2017-09-06 16:52:47 -0600

    open icon rotateing 180deg on click

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,22 @@
 (function(){
   console.log('Menu Initialized');
   var $menu = $('#menu')
+  var $openIcon = $('#open-menu')
 
   $('#menu-tab').on('click', function(e){
     var value = 0
     if($menu.css('left') === '0px'){
       value = -250
+      $openIcon.css('transform', 'rotate(0deg)')
+    }else{
+      $openIcon.css('transform', 'rotate(180deg)')
     }
+
     $menu.animate({
       left: `${value}px`
     }, 500)
+
+
   })
 
 }())
