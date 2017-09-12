8f8450f2e6432e054df1a4ed914c2ebf9eda9a61
Evan Busse <evanbusse@gmail.com>
2017-09-12 12:00:41 -0600

    added menu label and fade in/out effect

--- js/menu.js
+++ js/menu.js
@@ -2,33 +2,24 @@
   console.log('Menu Initialized');
   var $menu = $('#menu')
   var $openIcon = $('#open-menu')
+  var $menuLabel = $('.menu-label')
 
   $('#menu-tab').on('click', function(e){
     var value = 0
     if($menu.css('left') === '0px'){
       value = -250
       $openIcon.css('transform', 'rotate(0deg)')
+      $menuLabel.fadeOut(500)
     }else{
       $openIcon.css('transform', 'rotate(180deg)')
+      $menuLabel.fadeIn(500)
     }
 
     $menu.animate({
       left: `${value}px`
     }, 500)
 
-
   })
 
-  var topbar = document.getElementById('topbar')
-
-  console.log(topbar.offsetTop);
-
-  // $(window).on('scroll', (e) => {
-  //   console.log(window.scrollY);
-  //   if(window.scrollY === topbar.offsetTop){
-  //     console.log('BINGOOOOOO');
-  //     $('.total-center').css('position', 'fixed')
-  //   }
-  // })
 
 }())
